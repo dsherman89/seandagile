e7b29e60b3c33f8f8e802a309f07a8cd23ca18f1
Dan <n/a>
2023-04-18 10:39:24 +0100

    added some validation

--- code.js
+++ code.js
@@ -1,6 +1,28 @@
+function validateForm(){
 
-const myFunc = function(){
-    console.log("Dan2s");
+    // Get the form fields
+    const nameField = document.getElementById('uname');
+    const passwordField = document.getElementById('psw');
+
+    // Validate username
+    if (nameField.value.trim() === '') {
+        alert('Please enter your name.');
+        nameField.focus();
+        return false;
+    }
+
+    // Validate password
+    if (passwordField.value.trim() === '') {
+        alert('Please enter your password.');
+        passwordField.focus();
+        return false;
+    } else if (passwordField.value.trim().length < 8) {
+        alert('Password must be at least 8 characters long.');
+        passwordField.focus();
+        return false;
+    }
+
+    // Submit the form if all fields are valid
+    myForm.submit();
+    alert('form submitted');
 }
-
-exports.test1 = myFunc;
